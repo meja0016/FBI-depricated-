288e47c58de4d69df166c175fe25bc7c42c90f1f
Dr. Dr. Prof. h.c. Jason Nygel Petersson <116449318+1D8723@users.noreply.github.com>
2023-09-27 18:52:44 -0500

    Update nonPolice.sql

--- webservice/db/nonPolice.sql
+++ webservice/db/nonPolice.sql
@@ -1,8 +1,18 @@
-<nonPoliceProgram>
-<nonPoliceName>PENTAGON.EMEA</nonPoliceProgram>
-<nonPolicePorjects>
-<nonPoliceProject><nonPoliceName>JFK</nonPoliceName><nonPoliceAddress>ATOMCLOCK</nonPoliceAddress></nonPoliceProject>
-<nonPoliceProject><nonPoliceName>Meiers v. Meiers</nonPoliceName><nonPoliceAddress>REDHOUSE</nonPoliceAddress></nonPoliceProject>
-</nonPoliceProjects>
-  
-</nonPoliceProgram>
+CREATE TABLE nonPoliceProgram (
+    id int,
+    name varchar(255)
+);
+CREATE TABLE nonPoliceProject (
+    id int,
+    name varchar(255),
+    program_id int
+);
+
+#<nonPoliceProgram>
+#<nonPoliceName>PENTAGON.EMEA</nonPoliceProgram>
+#<nonPolicePorjects>
+#<nonPoliceProject><nonPoliceName>JFK</nonPoliceName><nonPoliceAddress>ATOMCLOCK</nonPoliceAddress></nonPoliceProject>
+#<nonPoliceProject><nonPoliceName>Meiers v. Meiers</nonPoliceName><nonPoliceAddress>REDHOUSE</nonPoliceAddress></nonPoliceProject>
+#</nonPoliceProjects> 
+#</nonPoliceProgram>
+
