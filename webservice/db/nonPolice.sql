a0a35961449a81041342f4fdf7bcd8adbcc21324
Dr. Dr. Prof. h.c. Jason Nygel Petersson <116449318+1D8723@users.noreply.github.com>
2023-09-27 19:04:28 -0500

    Update nonPolice.sql

--- webservice/db/nonPolice.sql
+++ webservice/db/nonPolice.sql
@@ -8,6 +8,19 @@ CREATE TABLE nonPoliceProject (
     program_id int
 );
 
+INSERT INTO nonPoliceProgram (id, name) VALUES (1, 'JFK');
+INSERT INTO nonPoliceProject (id, name) VALUES (2, 'Meiers v. Meiers');
+INSERT INTO nonPoliceProject (id, name) VALUES (3, 'Meiers v. Meiers');
+INSERT INTO nonPoliceProject (id, name) VALUES (4, 'COMPUTER');
+INSERT INTO nonPoliceProject (id, name) VALUES (3, 'REAL ESTATE');
+INSERT INTO nonPoliceProgram (id, name) VALUES (2, 'Chruch');
+INSERT INTO nonPoliceProgram (id, name) VALUES (5, 'USD');
+INSERT INTO nonPoliceProgram (id, name) VALUES (6, '1D8723');
+INSERT INTO nonPoliceProgram (id, name) VALUES (7, '');
+#db2 -c "select * from nonPoliceProgram"
+#db2 -tvf /tmp/nonPolice.sql
+
+
 #<nonPoliceProgram>
 #<nonPoliceName>PENTAGON.EMEA</nonPoliceProgram>
 #<nonPolicePorjects>
@@ -15,4 +28,3 @@ CREATE TABLE nonPoliceProject (
 #<nonPoliceProject><nonPoliceName>Meiers v. Meiers</nonPoliceName><nonPoliceAddress>REDHOUSE</nonPoliceAddress></nonPoliceProject>
 #</nonPoliceProjects> 
 #</nonPoliceProgram>
-
