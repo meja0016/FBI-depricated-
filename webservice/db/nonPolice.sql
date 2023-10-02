f4a9fc370d307863b9b023317492a60cfa53ca47
Dr. Dr. Prof. h.c. Jason Nygel Petersson <51761071+meja0016@users.noreply.github.com>
2023-10-02 13:04:28 -0500

    1.4 save

--- webservice/db/nonPolice.sql
+++ webservice/db/nonPolice.sql
@@ -8,6 +8,11 @@ CREATE TABLE nonPoliceProject (
     program_id int
 );
 
+create table nonPoliceTRX ( id int, name varchar(20),description varchar(120),year int)
+alter table nonPoliceTRX add column date TIMESTAMP with default current timestamp;
+INSERT INTO nonPoliceTRX (id, name,description,year) VALUES (1, '§Stalking','',2023);
+INSERT INTO nonPoliceTRX (id, name,description,year) VALUES (1, 'SQ.CDCR','SQ.CPVA.§187.PREA  IPX[].INT PRESIDENT.AVALABLITY (52 Deere Park Ave),thomas-is-dieter:wieland,Raik',2007);
+
 INSERT INTO nonPoliceProgram (id, name) VALUES (1, 'JFK');
 INSERT INTO nonPoliceProject (id, name) VALUES (2, 'Meiers v. Meiers');
 INSERT INTO nonPoliceProject (id, name) VALUES (3, 'Meiers v. Meiers');
@@ -16,10 +21,10 @@ INSERT INTO nonPoliceProject (id, name) VALUES (3, 'REAL ESTATE');
 INSERT INTO nonPoliceProgram (id, name) VALUES (2, 'Chruch');
 INSERT INTO nonPoliceProgram (id, name) VALUES (5, 'USD');
 INSERT INTO nonPoliceProgram (id, name) VALUES (6, '1D8723');
-INSERT INTO nonPoliceProgram (id, name) VALUES (7, '');
+INSERT INTO nonPoliceProgram (id, name) VALUES (7, 'COMMUNCATIONS');
+INSERT INTO nonPoliceProgram (id, name) VALUES (8, 'ATOMBOMB');
 #db2 -c "select * from nonPoliceProgram"
 #db2 -tvf /tmp/nonPolice.sql
-
 
 #<nonPoliceProgram>
 #<nonPoliceName>PENTAGON.EMEA</nonPoliceProgram>
@@ -28,3 +33,6 @@ INSERT INTO nonPoliceProgram (id, name) VALUES (7, '');
 #<nonPoliceProject><nonPoliceName>Meiers v. Meiers</nonPoliceName><nonPoliceAddress>REDHOUSE</nonPoliceAddress></nonPoliceProject>
 #</nonPoliceProjects> 
 #</nonPoliceProgram>
+
+#SQL1024N  A database connection does not exist
+#connect to npolice
