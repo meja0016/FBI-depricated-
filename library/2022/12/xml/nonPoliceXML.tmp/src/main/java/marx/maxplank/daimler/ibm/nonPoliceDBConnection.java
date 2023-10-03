7f9cca8eabeac8883275c73a99140f7d71339212
Dr. Dr. Prof. h.c. Jason Nygel Petersson <116449318+1D8723@users.noreply.github.com>
2023-10-03 18:00:26 +0000

    §2.1 Technology

---
+++ library/2022/12/xml/nonPoliceXML.tmp/src/main/java/marx/maxplank/daimler/ibm/nonPoliceDBConnection.java
@@ -0,0 +1,20 @@
+package marx.maxplank.daimler.ibm;
+
+public class nonPoliceDBConnection {
+    /*
+    nonPoliceDBConnection nonPoliceDBConnection = null;
+    public nonPoliceDBConnection(){
+
+    }
+
+    public nonPoliceDBConnection getInstance(){
+        if (nonPoliceDBConnection==null){
+            nonPoliceDBConnection=new nonPoliceDBConnection();
+        }
+        return  nonPoliceDBConnection nonPoliceDBConnection;
+
+    }
+    */
+    
+
+}
