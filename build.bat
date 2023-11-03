0b3d40f396152ba9a47b73c642bbe87f1a7be7e7
Dr. Dr. Prof. h.c. Jason Nygel Petersson <51761071+meja0016@users.noreply.github.com>
2023-11-03 14:51:34 -0500

    §1.4 save

--- build.bat
+++
@@ -1,6 +0,0 @@
-'https://download.oracle.com/java/20/latest/jdk-20_windows-x64_bin.zip
-
-sdk\bin\javac -cp sdk\lib/servlet-api.jar:. nonPoliceServlet.java
-#sdk\bin\javadoc -sourcepath src/ -classpath .\classes\   marx.maxplank.daimler.ibm
-timeout 15 > %tmp%/NULL
-
